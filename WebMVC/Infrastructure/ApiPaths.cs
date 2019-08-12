7f62746fd4ecc285c49fda3ec9e70ca6612abea3
Charles Hapgood-Strickland <charles.hapgood.strickland@gmail.com>
2019-08-12 19:41:07 +0200

    Create Custom Http Client to interface with API

---
+++ WebMVC/Infrastructure/ApiPaths.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Infrastructure
+{
+    public class ApiPaths
+    {
+        public static string GetAllCatalogItems(string baseUri, int page, int take,int? brandId, int? typeId)
+        {
+            var filterQs = "";
+
+            if(brandId.HasValue || typeId.HasValue)
+            {
+                var brandQs = (brandId.HasValue) ? brandId.Value.ToString() : "null";
+                var typeQs = (typeId.HasValue) ? typeId.Value.ToString() : "null";
+                filterQs = $"/type/{typeQs}/brand/{brandQs}";
+            }
+
+            return $"{baseUri}/items/{filterQs}?pageIndex={page}&pageSize={take}";
+        }
+
+        public static string GetCatalogItem(string baseUri, int Id)
+        {
+            return $"{baseUri}/items/{Id}";
+        }
+
+        public static string GetAllTypes(string baseUri)
+        {
+            return $"{baseUri}/catalogTypes";
+        }
+
+        public static string GetAllBrands(string baseUri)
+        {
+            return $"{baseUri}/catalogBrands";
+        }
+    }
+}
---
+++ WebMVC/Infrastructure/CustomHttpClient.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebMVC.Infrastructure
+{
+    public class CustomHttpClient : IHttpClient
+    {
+        private HttpClient _client;
+        private ILogger<CustomHttpClient> _logger;
+
+
+        public CustomHttpClient(ILogger<CustomHttpClient> logger)
+        {
+            _client = new HttpClient();
+            _logger = logger;
+        }
+
+        public async Task<HttpResponseMessage> DeleteAsync(string uri)
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Delete, uri);
+            var response = await _client.SendAsync(requestMessage);
+            return response;
+        }
+
+        public async Task<string> GetStringAsync(string uri)
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+            var response = await _client.SendAsync(requestMessage);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public Task<HttpResponseMessage> PostAsync<T>(string uri, T item)
+        {
+            return DoPostPutAsync(HttpMethod.Post, uri, item);
+        }
+
+        public Task<HttpResponseMessage> PutAsync<T>(string uri, T item)
+        {
+            return DoPostPutAsync(HttpMethod.Put, uri, item);
+        }
+
+        private async Task<HttpResponseMessage> DoPostPutAsync<T>(HttpMethod post, string uri, T item)
+        {
+
+            var requestMessage = new HttpRequestMessage(post, uri)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(item), System.Text.Encoding.UTF8, "application/json")
+            };
+
+            var response = await _client.SendAsync(requestMessage);
+
+            if(response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+            {
+                throw new HttpRequestException();
+            }
+
+            return response;
+        }
+    }
+}
